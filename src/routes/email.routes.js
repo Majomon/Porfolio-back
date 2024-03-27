78a1625ea50b00ebb551e9244f841dcbd544aa90
Mauri <mauri.monzon91@gmail.com>
2024-03-27 14:22:35 -0300

    as

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -2,6 +2,10 @@ const routerEmails = require("express").Router();
 
 const handlerEmail = require("../handler/handlerEmail");
 
+routerEmails.get("/", (req, res) => {
+  res.status(200).json(email);
+});
+
 routerEmails.post("/", handlerEmail);
 
 module.exports = routerEmails;
